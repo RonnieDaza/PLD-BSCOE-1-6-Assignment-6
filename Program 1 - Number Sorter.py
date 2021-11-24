26042af4a0682f642195d855af14b989c0241448
unknown <daza.ronnie.09212002@gmail.com>
2021-11-24 16:39:39 +0800

    Def orderOfallnumbers Commit

---
+++ Program 1 - Number Sorter.py
@@ -0,0 +1,9 @@
+# Program 1: Number Sorter
+# Create a program that ask 4 numbers. 
+# Print the 4 numbers from highest to lowest using only if-else statement.
+
+def orderOfallnumbers():
+    firstNumber = float(input("Enter the first number: "))
+    secondNumber = float(input("Enter the second number: "))
+    thirdNumber = float(input("Enter the third number: "))
+    fourthNumber = float(input("Enter the fourth number: "))
