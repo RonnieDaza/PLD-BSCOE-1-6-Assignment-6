ccdc265b8c25863a6d7b04018c8467227f92430d
unknown <daza.ronnie.09212002@gmail.com>
2021-11-24 17:32:27 +0800

    Define the second random number from 0 to 99 using random.randint function Commit

--- Program 2 - Addition Quiz.py
+++ Program 2 - Addition Quiz.py
@@ -16,3 +16,5 @@ userScore = 0
 for integer in range(10):
     # 5. Define the first random number from 0 to 99 using random.randint function.
     integer_firstNumber = random.randint(0, 99)
+    # 6. Define the second random number from 0 to 99 using random.randint function. 
+    integer_secondNumber = random.randint(0, 99)
