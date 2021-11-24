613ab670ddf9e7d08bef0da7c98c7e6d5d4d2480
unknown <daza.ronnie.09212002@gmail.com>
2021-11-24 17:29:34 +0800

    Import random module Commit

---
+++ Program 2 - Addition Quiz.py
@@ -0,0 +1,9 @@
+# Program 2: Addition Quiz
+# Create a program that will automatically generate two random numbers to add (0 to 99).
+# Let the user answer and evaluate if the user has the correct answer.
+# The program will ask the user 10 addition operation.
+# Display the result summary of the 10 operations (Example: 9/10).
+
+# Steps
+# 1. Import random module.
+import random
