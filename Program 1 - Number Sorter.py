35ee249ae2df5776c4cdf97b55e9d201de3edf5a
unknown <daza.ronnie.09212002@gmail.com>
2021-11-24 17:00:14 +0800

    Change all the word 'print' to 'display' Commit

--- Program 1 - Number Sorter.py
+++ Program 1 - Number Sorter.py
@@ -13,31 +13,31 @@ def orderOfallnumbers():
     if firstNumber > secondNumber and firstNumber > thirdNumber and firstNumber > fourthNumber:
         # 2. If it is true, then test if the second number is greater than the third number and fourth number.
         if secondNumber > thirdNumber and secondNumber > fourthNumber:
-            # 3. If it is true, then test if the third number is greater than the fourth number. Print the respective order.
+            # 3. If it is true, then test if the third number is greater than the fourth number. Display the respective order.
             if thirdNumber > fourthNumber:
                 orderOfallnumbers = firstNumber, secondNumber, thirdNumber, fourthNumber
             else:
-                # 4. If it is false, then the fourth number is greater than the third number. Print the respective order.
+                # 4. If it is false, then the fourth number is greater than the third number. Display the respective order.
                 orderOfallnumbers = firstNumber, secondNumber, fourthNumber, thirdNumber
             # 5. Return the orderOfallnumbers.
             return orderOfallnumbers
         # 6. If the previous statements are false, then test if the third number is greater than the second number and fourth number.
         elif thirdNumber > secondNumber and thirdNumber > fourthNumber:
-            # 7. If it is true, then test if the second number is greater than the fourth number. Print the respective order.
+            # 7. If it is true, then test if the second number is greater than the fourth number. Display the respective order.
             if secondNumber > fourthNumber:
                 orderOfallnumbers = firstNumber, thirdNumber, secondNumber, fourthNumber
             else:
-                # 8. If it is false, then the fourth number is greater than the second number. Print the respective order.
+                # 8. If it is false, then the fourth number is greater than the second number. Display the respective order.
                 orderOfallnumbers = firstNumber, thirdNumber, fourthNumber, secondNumber
             # 9. Return the orderOfallnumbers.
             return orderOfallnumbers
         else:
             # 10. If nothing matches the previous descriptions, then the fourth number is the second largest number.
-            # 11. Test if the third number is greater than the second number. Print the respective order.
+            # 11. Test if the third number is greater than the second number. Display the respective order.
             if thirdNumber > secondNumber:
                 orderOfallnumbers = firstNumber, fourthNumber, thirdNumber, secondNumber
             else:
-                # 12. If it is false, then the second number is greater than the third number. Print the respective order.
+                # 12. If it is false, then the second number is greater than the third number. Display the respective order.
                 orderOfallnumbers = firstNumber, fourthNumber, secondNumber, thirdNumber
             # 13. Return the orderOfallnumbers.
             return orderOfallnumbers
@@ -47,26 +47,26 @@ def orderOfallnumbers():
     if secondNumber > firstNumber and secondNumber > thirdNumber and secondNumber > fourthNumber:
         # 15. If it is true, then test if the first number is greater than the third number and the fourth number.
         if firstNumber > thirdNumber and firstNumber > fourthNumber:
-            # 16. If it is true, then test if the third number is greater than the fourth number. Print the respective order.
+            # 16. If it is true, then test if the third number is greater than the fourth number. Display the respective order.
             if thirdNumber > fourthNumber:
                 orderOfallnumbers = secondNumber, firstNumber, thirdNumber, fourthNumber
             else:
-                # 17. If it is false, then the fourth number is greater than the third number. Print the respective order.
+                # 17. If it is false, then the fourth number is greater than the third number. Display the respective order.
                 orderOfallnumbers = secondNumber, firstNumber, fourthNumber, thirdNumber
             # 18. Return the orderOfallnumbers.
             return orderOfallnumbers
         # 19. If the previous statements are false, then test if the third number is greater than the first number and the fourth number.
         elif thirdNumber > firstNumber and thirdNumber > fourthNumber:
-            # 20. If it is true, then test if the first number is greater than the fourth number. Print the respective order.
+            # 20. If it is true, then test if the first number is greater than the fourth number. Display the respective order.
             if firstNumber > fourthNumber:
                 orderOfallnumbers = secondNumber, thirdNumber, firstNumber, fourthNumber
             else:
-                # 21. If it is false, then the fourth number is greater than the first number. Print the respective order.
+                # 21. If it is false, then the fourth number is greater than the first number. Display the respective order.
                 orderOfallnumbers = secondNumber, thirdNumber, fourthNumber, firstNumber
             # 22. Return the orderOfallnumbers.
             return orderOfallnumbers
         else:
             # 23. If nothing matches the previous descriptions, then the fourth number is the second largest number.
-            # 24. Test if the third number is greater than the first number. Print the respective order.
+            # 24. Test if the third number is greater than the first number. Display the respective order.
             if thirdNumber > firstNumber:
                 orderOfallnumbers = secondNumber, fourthNumber, thirdNumber, firstNumber
